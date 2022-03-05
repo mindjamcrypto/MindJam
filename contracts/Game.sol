dc6a46a969be64b1ceddfc8b2a015029ac4c4fdc
pdomenico <domenicoprocopio.a@gmail.com>
2022-03-05 12:24:25 +0100

    refined some functions and added a withdraw function

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -10,10 +10,13 @@ contract Game {
     }
 
     Session[] sessions;
+    mapping(address => uint256) playerLastSessionId;
     uint256 hintPrice;
     uint256 wordRevealPrice;
     address owner;
 
+    event NewSessionStarted(address from, uint256 sessionId);
+    event SessionEnded(address from, uint256 sessionId);
     event RequestHint(address from, uint256 sessionId);
     event RequestWordReveal(address from, uint256 sessionId);
 
@@ -40,19 +43,37 @@ contract Game {
             true
         );
         sessions.push(session);
+        playerLastSessionId[msg.sender] = session.id;
+        emit NewSessionStarted(msg.sender, session.id);
     }
 
     function requestHint(uint256 _sessionId) public payable {
         Session memory session = sessions[_sessionId]; // gas saver
-        require(session.player == msg.sender && session.active);
-        require(msg.value >= hintPrice);
+        require(
+            session.player == msg.sender,
+            "Only the player can request hints!"
+        );
+        require(session.active, "This session has ended");
+        require(
+            msg.value >= hintPrice,
+            "Insufficient amount to cover hint cost"
+        );
+
         emit RequestHint(msg.sender, _sessionId);
     }
 
     function requestWordReveal(uint256 _sessionId) public payable {
         Session memory session = sessions[_sessionId]; // gas saver
-        require(session.player == msg.sender && session.active);
-        require(msg.value >= wordRevealPrice);
+        require(
+            session.player == msg.sender,
+            "Only the player can request hints!"
+        );
+        require(session.active, "This session has ended");
+        require(
+            msg.value >= wordRevealPrice,
+            "Insufficient amount to cover word reveal cost"
+        );
+
         emit RequestWordReveal(msg.sender, _sessionId);
     }
 
@@ -61,10 +82,15 @@ contract Game {
      */
     function endSession(uint256 _sessionId) public returns (uint256 timeTaken) {
         Session memory session = sessions[_sessionId]; // gas saver
-        require(session.active && session.player == msg.sender);
+        require(session.active, "The session has already ended!");
+        require(
+            session.player == msg.sender,
+            "Only the player can terminate the session"
+        );
 
         sessions[_sessionId].active = false;
         timeTaken = block.timestamp - session.timestamp;
+        emit SessionEnded(msg.sender, _sessionId);
     }
 
     // Some helper methods
@@ -104,5 +130,19 @@ contract Game {
 
     function setWordRevealPrice(uint256 _price) public onlyOwner {
         wordRevealPrice = _price;
+    }
+
+    function getPlayerLastSessionId(address _player)
+        public
+        view
+        returns (uint256)
+    {
+        return playerLastSessionId[_player];
+    }
+
+    // Withdraw the funds
+    function withdraw(uint256 _amount, address payable _to) public onlyOwner {
+        (bool sent, ) = _to.call{value: _amount}("");
+        require(sent, "Transaction failed!");
     }
 }
