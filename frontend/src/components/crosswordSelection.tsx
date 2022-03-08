a051a3861f463402253ec628d4a46b66f764f5db
jordang7 <77jgreenberg77@gmail.com>
2022-03-08 12:26:42 -0500

    add image files and selection component

---
+++ frontend/src/components/crosswordSelection.tsx
@@ -0,0 +1,90 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Link,
+  Heading,
+} from "@chakra-ui/react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { Link as ReactRouter } from "react-router-dom";
+import axios from "axios";
+import { Error } from "../components/error";
+import { ClueTypeOriginal } from "@jaredreisinger/react-crossword/dist/types";
+interface RevealSquares {
+  row: number;
+  col: number;
+  letter: string;
+}
+interface revealWord {
+  row: number;
+  col: number;
+  direction: string;
+  word: string;
+}
+type mongoFormat = {
+  _id: string;
+  revealSquares: Array<RevealSquares>;
+  across: Record<string, ClueTypeOriginal>;
+  down: Record<string, ClueTypeOriginal>;
+  title: string;
+  revealWords: Array<revealWord>;
+};
+
+export const CrosswordSelection = () => {
+  const [crosswordData, setCrosswordData] = useState<Array<mongoFormat>>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        await axios.get("http://localhost:3001/crosswords").then((result) => {
+          //console.log(result.data);
+          setCrosswordData(result.data);
+        });
+      } catch (e) {
+        return <Error />;
+      }
+    }
+    fetchData();
+  }, []);
+  return (
+    <Box
+      w="full"
+      bg="whiteAlpha.900"
+      px={{
+        base: "50px", // 0-48em
+        md: "100px", // 48em-80em,
+        xl: "200px", // 80em+
+      }}
+      py="60px"
+    >
+      <Flex justifyContent="center" alignItems="center">
+        <Heading
+          fontSize={{
+            base: 20, // 0-48em
+            md: 44, // 48em-80em,
+            xl: 54, // 80em+
+          }}
+          letterSpacing="6px"
+          pb="15px"
+        >
+          Choose a crossword puzzle!
+        </Heading>
+      </Flex>
+      <Flex justifyContent="center" alignItems="center">
+        <Grid templateColumns="repeat(4, 1fr)" gap={12}>
+          {crosswordData?.map((puzzle) => (
+            <GridItem>
+              <Button>
+                <Link as={ReactRouter} to={`/crossword/${puzzle._id}`}>
+                  {puzzle.title}
+                </Link>
+              </Button>
+            </GridItem>
+          ))}
+        </Grid>
+      </Flex>
+    </Box>
+  );
+};
